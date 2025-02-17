5a8297d91cda914ebde9a734518338a598ad6b2b
Arthur Marinho <arthursnat@gmail.com>
2025-02-17 15:19:59 -0300

    Add stylesheet links to header ,footer, register, teams, proplayers, and about us and implement SearchBar component

---
+++ components/ui/SearchBar.js
@@ -0,0 +1,11 @@
+const SearchBar = () =>{
+  return(
+    <div>
+        <div class="rightnavbar">
+          <input type="search" placeholder="Search..."/>
+        </div>  
+    </div>
+  )
+}
+
+export default SearchBar
