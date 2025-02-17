4838792a2e0a28dde4b14239c852ed1da3a9c64b
Arthur Marinho <arthursnat@gmail.com>
2025-02-17 15:34:04 -0300

    Update player data and fix stylesheet links in views

--- routes/proplayers.js
+++ routes/proplayers.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     const players = [
-       {name:"Leonardo", image:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTKjkxoQ9bYCemjt7HCuZ9qKPGbeoehly7P0nC_ASWbzHhjP-kY9CqSgcNFcNHr32yvG5zF79jz6V9XHTodcnkUJw", game:"CS:GO"},
-       {name:"Leonardo", image:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTKjkxoQ9bYCemjt7HCuZ9qKPGbeoehly7P0nC_ASWbzHhjP-kY9CqSgcNFcNHr32yvG5zF79jz6V9XHTodcnkUJw", game:"CS:GO"},
-       {name:"Leonardo", image:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTKjkxoQ9bYCemjt7HCuZ9qKPGbeoehly7P0nC_ASWbzHhjP-kY9CqSgcNFcNHr32yvG5zF79jz6V9XHTodcnkUJw", game:"CS:GO"},
+       {name:"Pro", image:"https://cdn-icons-png.flaticon.com/512/157/157776.png", game:"CS:GO"},
+       {name:"Pro", image:"https://cdn-icons-png.flaticon.com/512/157/157776.png", game:"CS:GO"},
+       {name:"Pro", image:"https://cdn-icons-png.flaticon.com/512/157/157776.png", game:"CS:GO"},
     ];
 
     res.render("proplayers", { players });
